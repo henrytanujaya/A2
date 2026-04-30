92e63406d2f8e71507f913ac78b6225244dc53f3
Neroneko-cyber <projectabcde306@gmail.com>
2026-04-30 12:14:25 +0700

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8321',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:8321',
+        changeOrigin: true,
+      },
+    }
   }
 })
